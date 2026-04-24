b1c1e4472852bc1433bfb003b8531d36073984ed
a111182112-ship-it <a111182112@nkust.edu.tw>
2026-04-24 17:46:46 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    Base:'/a111182112-ship-it/',
+    Base:'/personal-website/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
